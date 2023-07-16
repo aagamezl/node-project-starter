c1917dfe581dddaf8f6513b7244ab7e0820bc011
Álvaro José Agámez Licha <alvaroagamez@outlook.com>
2023-07-16 21:04:22 +0200

    feat: initial commit

---
+++ ava.config.js
@@ -0,0 +1,13 @@
+export default {
+  verbose: true,
+  failFast: true,
+  extensions: [
+    'js'
+  ],
+  files: [
+    'test/**/*.spec.js'
+  ],
+  ignoredByWatcher: [
+    'temp'
+  ]
+}
---
+++ test/starter.spec.js
@@ -0,0 +1,5 @@
+import test from 'ava'
+
+test('should pass the first starter test', t => {
+  t.pass()
+})
