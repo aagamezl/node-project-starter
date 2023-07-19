99afc45ce2fee9bf414d03fd17e768f4f6b7722f
Álvaro José Agámez Licha <alvaroagamez@outlook.com>
2023-07-19 19:34:43 +0200

    feat(jsdoc): add jsdoc type checking and generation

---
+++ src/sum.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+export const sum = (a, b) => {
+  return a + b
+}
--- test/starter.spec.js
+++ test/starter.spec.js
@@ -1,5 +1,7 @@
 import test from 'ava'
 
+import { sum } from '../src/sum.js'
+
 test('should pass the first starter test', t => {
-  t.pass()
+  t.is(sum(1, 2), 3)
 })
